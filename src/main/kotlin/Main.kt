3ace1c02aab85f6edfb8b204680b170c2490a1de
Sergey Makletsov <smaklets@kpfu.ru>
2023-11-08 17:30:21 +0300

    Выделение мышкой

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,34 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.PointerMatcher
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import drawing.FractalPainter
+import drawing.SelectionRect
 import drawing.convertation.Plane
 import math.fractals.Mandelbrot
 import kotlin.math.*
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 @Preview
 fun App() {
+    var rect by mutableStateOf(SelectionRect(Offset.Zero))
     val fp = FractalPainter(Mandelbrot){
         if (it == 1f) Color.Black
         else {
@@ -27,10 +40,21 @@ fun App() {
     }
     fp.plane = Plane(-2.0, 1.0, -1.0, 1.0, 0f, 0f)
     MaterialTheme {
-        Canvas(Modifier.fillMaxSize().padding(8.dp)){
+        Canvas(Modifier.fillMaxSize().padding(8.dp).pointerInput(Unit){
+            detectDragGestures(
+                onDragStart = {
+                    rect = SelectionRect(it)
+                              },
+                onDrag = {
+                    rect.addPoint(it)
+                         },
+                matcher = PointerMatcher.Primary)
+        }){
             fp.width = size.width.toInt()
             fp.height = size.height.toInt()
             fp.paint(this)
+            drawRect(Color(1f, 1f, 1f, 0.3f), rect.topLeft, rect.size)
+
         }
     }
 }
---
+++ src/main/kotlin/drawing/SelectionRect.kt
@@ -0,0 +1,29 @@
+package drawing
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.min
+
+class SelectionRect(p: Offset) {
+    private var pt1 by mutableStateOf<Offset>(p)
+    private var sz by mutableStateOf(Size.Zero)
+    val topLeft: Offset
+        get() = Offset(
+            min(pt1.x, pt1.x + sz.width),
+            min(pt1.y, pt1.y + sz.height)
+        )
+    val size: Size
+        get() = Size(
+            sz.width.absoluteValue,
+            sz.height.absoluteValue
+        )
+    fun addPoint(p:Offset)
+    {
+        sz = Size(sz.width + p.x, sz.height + p.y)
+    }
+}
