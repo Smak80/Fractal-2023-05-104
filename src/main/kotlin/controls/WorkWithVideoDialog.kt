34a2f76487535c4e3f7724b6670679d7fcc9fb20
Lev Grekov <levgrekov@mail.ru>
2023-11-25 23:37:29 +0300

    Начал делать интерфейс для VideoMakinga. Поручил Булату доделать меню: Сделать Интерфейс для Выбора параметров Видео

--- src/main/kotlin/controls/WorkWithVideoDialog.kt
+++ src/main/kotlin/controls/WorkWithVideoDialog.kt
@@ -1,22 +1,16 @@
 package controls
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,12 +18,7 @@ import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 
 @Composable
-fun workWithVideoDialog(close:()->Unit) {
-    var height by remember { mutableStateOf(0) }
-    var width by remember { mutableStateOf(0) }
-    var fps by remember { mutableStateOf(0) }
-    var duration by remember { mutableStateOf(0) }
-
+fun workWithVideoDialog(){
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -38,125 +27,14 @@ fun workWithVideoDialog(close:()->Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 2.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            // Текст в диалоговом окне
-            Text(
-                "Параметры Видео",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.weight(1f),
-            )
-            //Закрытие
-            IconButton(
-                onClick = close,
-                modifier = Modifier
-                    .padding(horizontal = 8.dp),
-            ) {
-                Icon(
-                    Icons.Default.Close,
-                    contentDescription = "Закрыть",
-                )
-            }
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 2.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            intTextField(
-                value = height,
-                label = "Высота",
-                onValueChange = {height=it},
-                maxValue = 10000,
-                modifier = Modifier.weight(1f)
-            )
-            intTextField(
-                value = width,
-                label = "Ширина",
-                onValueChange = {width=it},
-                maxValue = 10000,
-                modifier = Modifier.weight(1f)
-            )
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 2.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            intTextField(
-                value = fps,
-                label = "FPS",
-                onValueChange = {fps=it},
-                minValue = 1,
-                maxValue = 240,
-                modifier = Modifier.weight(1f)
-            )
-            intTextField(
-                value = duration,
-                label = "Длительность, c",
-                onValueChange = {duration=it},
-                maxValue = 10*60*60,
-                modifier = Modifier.weight(1f)
-            )
-        }
+        // Текст в диалоговом окне
+        Text(
+            "Настройки Видео",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(5.dp).padding(top = 10.dp)
+        )
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 2.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Button(
-                onClick = {
-
-                },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(16.dp)
-                    .height(45.dp)
-            ) {
-                Text("Создать")
-            }
-            Button(
-                onClick = {
-
-                },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(16.dp)
-                    .height(45.dp)
-            ) {
-                Text("Очистить")
-            }
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 2.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ){
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp, vertical = 2.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ){
-
-        }
     }
 }
