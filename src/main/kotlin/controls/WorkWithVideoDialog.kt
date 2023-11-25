b2b423e87fd780aae9a990b3e6f7e653edb6cc32
Bulat671 <bulatabdrahmanov44@gmail.com>
2023-11-26 01:29:08 +0300

    сделал чтобы открывалось окно для создания Видео.

--- src/main/kotlin/controls/WorkWithVideoDialog.kt
+++ src/main/kotlin/controls/WorkWithVideoDialog.kt
@@ -1,15 +1,14 @@
 package controls
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -27,14 +26,62 @@ fun workWithVideoDialog(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Текст в диалоговом окне
-        Text(
-            "Настройки Видео",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(5.dp).padding(top = 10.dp)
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 2.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Текст в диалоговом окне
+            Text(
+                "Параметры Видео",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(1f),
+            )
+            //Закрытие
+            IconButton(
+                onClick = {
+                },
+                modifier = Modifier
+                    .padding(horizontal = 8.dp),
+            ) {
+                Icon(
+                    Icons.Default.Close,
+                    contentDescription = "Закрыть",
+                )
+            }
+        }
 
+        // Кнопки в строку
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp, vertical = 2.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // Кнопка "Изображение"
+            Button(
+                onClick = {
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp)
+            ) {
+                Text("Изображение")
+            }
+            // Кнопка "Фрактал"
+            Button(
+                onClick = {
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp)
+            ) {
+                Text("Фрактал")
+            }
+        }
     }
 }
