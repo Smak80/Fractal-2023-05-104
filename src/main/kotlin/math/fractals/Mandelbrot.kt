675fd0453d6127993f474298ae46f4aa2bd6b62b
KarinaRashidova <karinarashidova28@mail.ru>
2023-11-30 16:33:50 +0300

    создала фрактал JuliaSet

--- src/main/kotlin/math/fractals/Mandelbrot.kt
+++ src/main/kotlin/math/fractals/Mandelbrot.kt
@@ -5,7 +5,7 @@ import math.Complex
 object Mandelbrot : AlgebraicFractal {
     override var maxIterations: Int = 5000
         set(value) { field = value.coerceIn(20..10000)}
-    var r = 2.0
+    private var r = 2.0
     override fun isInSet(c: Complex): Float {
         val z = Complex()
         val r2 = r*r
