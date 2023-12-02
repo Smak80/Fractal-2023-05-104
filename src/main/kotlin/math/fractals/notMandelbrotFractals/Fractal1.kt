aac2c4a516e587e9bc53e23df89d67556c2b9c8d
marat <marat-mmb@mail.ru>
2023-12-03 00:06:43 +0300

    Создал два объекта Fractal1 и Fractal2. Добавил в Main.kt новые fp, их переделать

---
+++ src/main/kotlin/math/fractals/notMandelbrotFractals/Fractal1.kt
@@ -0,0 +1,21 @@
+package math.fractals.notMandelbrotFractals
+
+import math.Complex
+import math.fractals.AlgebraicFractal
+
+object Fractal1 : AlgebraicFractal {
+    override var maxIterations: Int = 5000
+        set(value) { field = value.coerceIn(20..10000)}
+    var r = 2.0
+    override fun isInSet(c: Complex): Float {
+        val z = Complex()
+        val r2 = r*r
+        for (i in 1..maxIterations){
+            z*=z * z * z * z
+            z+=c
+            if (z.abs2() >= r2)
+                return i.toFloat()/ maxIterations
+        }
+        return 1f
+    }
+}
