9f809fa0fa5c0e428118b6945658fe3443286a9e
marat <marat-mmb@mail.ru>
2023-12-07 00:56:12 +0300

    Создал файлы с лямбда функциями отрисовки и фракталами

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,43 +15,61 @@ import androidx.compose.ui.window.application
 import drawing.FractalPainter
 import drawing.Painter
 import drawing.SelectionRect
+import drawing.colors.colors
 import drawing.convertation.Converter
 import drawing.convertation.Plane
-import math.fractals.Mandelbrot
-import kotlin.math.*
+import math.fractals.Fractal
+import math.fractals.funcs
+
 
 @Composable
 @Preview
 fun App() {
-    //потом придумать, что с этими лямбдами делать
-//    val fp = remember { FractalPainter(Mandelbrot){
-//        if (it == 1f) Color.Black
-//        else {
-//            val r = sin(it*15f).absoluteValue
-//            val g = (sin(-8f*it)* cos(it*5f+12f)).absoluteValue
-//            val b = log2(2f - cos(sin(18*-it)))
-//            Color(r, g, b)
-//        }
-//    }}
 
-//    val fp = remember { FractalPainter(Mandelbrot){
-//        if (it == 1f) Color.Black
-//        else {
-//            val r = (2*asin(it + PI*(tan(it)))/PI).absoluteValue.toFloat()
-//            val g = (2* atan(it + PI*(1-cos(it))) / PI).absoluteValue.toFloat()
-//            val b = (2*acos(it+ PI*(1-sin(it)))/PI).absoluteValue.toFloat()
-//            Color(r, g, b)
+//    val funcs: Map<String, (Complex) -> Complex> = mapOf(
+//        "Mandelbrot" to {z: Complex -> z},
+//        "square" to {z:Complex -> z * z * z * z * z},
+//        "third_pow" to {z:Complex -> z * z * z},
+//        "multiply_and_plus" to {z:Complex -> z * z * z * z * z + z * z * z * z}
+//    )
+
+//    val colors: Map<String, (Float) -> Color> = mapOf(
+//
+//        "color1" to {
+//            if (it == 1f) Color.Black
+//            else {
+//                val r = sin(it*15f).absoluteValue
+//                val g = (sin(-8f*it)* cos(it*5f+12f)).absoluteValue
+//                val b = log2(2f - cos(sin(18*-it)))
+//                Color(r, g, b)
+//            }
+//        },                                      // стандартная раскраска с пары
+//
+//        "color2" to {
+//            if (it == 1f) Color.Black
+//            else {
+//                val r = cos(it + PI*(0.5 + it)).absoluteValue.toFloat()
+//                val g = (2*atan(it + PI*(tan(it)))/ PI).absoluteValue.toFloat()
+//                val b = cos(it+PI*(0.5+sin(it))).absoluteValue.toFloat()
+//                Color(r, g, b)
+//            }
+//        },
+//
+//        "color3" to {
+//            if (it == 1f) Color.Black
+//            else {
+//                val r = (0.5f * (1 - cos(16f * it * it)) * sin(-12*it)).absoluteValue
+//                val g = sin(5f * it).absoluteValue  * cos(5f * it).absoluteValue
+//                val b = log10(1f + 5 * it)
+//                Color(r, g, b)
+//            }
 //        }
-//    }}
-    val fp = remember { FractalPainter(Mandelbrot){
-        if (it == 1f) Color.Black
-        else {
-            val r = cos(it + PI*(0.5 + it)).absoluteValue.toFloat()
-            val g = (2*atan(it + PI*(tan(it)))/ PI).absoluteValue.toFloat()
-            val b = cos(it+PI*(0.5+sin(it))).absoluteValue.toFloat()
-            Color(r, g, b)
-        }
-    }}
+//    )
+
+    Fractal.function = funcs["Mandelbrot"]!!
+
+    val fp = remember { FractalPainter(Fractal, colors["color2"]!!)}
+
     fp.plane = Plane(-2.0, 1.0, -1.0, 1.0, 0f, 0f)
 
 
