a4809b11fbe01dfc6f1ecfa58bf28e2cb73d754b
Amwillav <velitova03@gmail.com>
2023-12-07 23:47:03 +0300

    Почему-то не работает моментальная загрузка фрактала

--- src/main/kotlin/tools/FractalDataProcessor.kt
+++ src/main/kotlin/tools/FractalDataProcessor.kt
@@ -11,50 +11,48 @@ import javax.swing.filechooser.FileNameExtensionFilter
 import javax.swing.filechooser.FileSystemView
 
 object FractalDataProcessor {
+    fun saveFractal(data: FractalData){
+        val fileSystemView = FileSystemView.getFileSystemView()
+        val fileChooser = JFileChooser(fileSystemView.defaultDirectory, fileSystemView).apply {
+            dialogTitle = "Сохранение состояния фрактала"
+            fileFilter = FileNameExtensionFilter(".fractal", "fractal")
+            isAcceptAllFileFilterUsed = false
+            fileSelectionMode = JFileChooser.OPEN_DIALOG
+        }
+        val openDialogResult = fileChooser.showSaveDialog(fileChooser)
+        if (openDialogResult == JFileChooser.APPROVE_OPTION) {
+            val fileAbsolutePath =
+                fileChooser.currentDirectory.absolutePath + "\\" +
+                        fileChooser.selectedFile.nameWithoutExtension + ".fractal"
+            val fileStream = FileOutputStream(fileAbsolutePath)
+            val objectStream = ObjectOutputStream(fileStream)
+            objectStream.writeObject(data)
+            objectStream.close()
+            JOptionPane.showMessageDialog(fileChooser, "Файл '$fileAbsolutePath' успешно сохранен")
+        }
+    }
 
-    object FractalDataProcessor {
-        fun saveFractal(data: FractalData){
-            val fileSystemView = FileSystemView.getFileSystemView()
-            val fileChooser = JFileChooser(fileSystemView.defaultDirectory, fileSystemView).apply {
-                dialogTitle = "Сохранение состояния фрактала"
-                fileFilter = FileNameExtensionFilter(".fractal", "fractal")
-                isAcceptAllFileFilterUsed = false
-                fileSelectionMode = JFileChooser.OPEN_DIALOG
-            }
-            val openDialogResult = fileChooser.showSaveDialog(fileChooser)
-            if (openDialogResult == JFileChooser.APPROVE_OPTION) {
-                val fileAbsolutePath =
-                    fileChooser.currentDirectory.absolutePath + "\\" +
-                            fileChooser.selectedFile.nameWithoutExtension + ".fractal"
-                val fileStream = FileOutputStream(fileAbsolutePath)
-                val objectStream = ObjectOutputStream(fileStream)
-                objectStream.writeObject(data)
-                objectStream.close()
-                JOptionPane.showMessageDialog(fileChooser, "Файл '$fileAbsolutePath' успешно сохранен")
+    fun loadData() :  FractalData? {
+        val fileChooser = JFileChooser().apply {
+            dialogTitle = "Открытие фрактала"
+            fileFilter = FileNameExtensionFilter(".fractal", "fractal")
+            isAcceptAllFileFilterUsed = false
+            fileSelectionMode = JFileChooser.OPEN_DIALOG
+        }
+        val openDialogResult = fileChooser.showOpenDialog(fileChooser)
+        if (openDialogResult == JFileChooser.APPROVE_OPTION) {
+            val fileAbsolutePath = fileChooser.selectedFile.absolutePath
+            val fileStream = FileInputStream(fileAbsolutePath)
+            val objectStream = ObjectInputStream(fileStream)
+            val result = objectStream.readObject() as? FractalData
+            return if ( result is FractalData){
+                JOptionPane.showMessageDialog(fileChooser, "\"Файл '$fileAbsolutePath' успешно открыт\"")
+                result
+            } else{
+                JOptionPane.showMessageDialog(fileChooser, "Error")
+                null
             }
         }
-
-        fun loadData() :  FractalData? {
-            val fileChooser = JFileChooser().apply {
-                dialogTitle = "Открытие фрактала"
-                fileFilter = FileNameExtensionFilter(".fractal", "fractal")
-                isAcceptAllFileFilterUsed = false
-                fileSelectionMode = JFileChooser.OPEN_DIALOG
-            }
-            val openDialogResult = fileChooser.showOpenDialog(fileChooser)
-            if (openDialogResult == JFileChooser.APPROVE_OPTION) {
-                val fileAbsolutePath = fileChooser.selectedFile.absolutePath
-                val fileStream = FileInputStream(fileAbsolutePath)
-                val objectStream = ObjectInputStream(fileStream)
-                val result = objectStream.readObject() as? FractalData
-                return if ( result is FractalData){
-                    result
-                } else{
-                    JOptionPane.showMessageDialog(fileChooser, "Error")
-                    null
-                }
-            }
-            return null
-        }
+        return null
     }
 }
