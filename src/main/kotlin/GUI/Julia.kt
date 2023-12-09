59ed37f0356d9e0e970a33fa1e358189a995cc8a
marat <marat-mmb@mail.ru>
2023-12-09 21:24:03 +0300

    что-то еще

--- src/main/kotlin/GUI/Julia.kt
+++ src/main/kotlin/GUI/Julia.kt
@@ -1,46 +1,99 @@
 package GUI
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.PointerMatcher
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 import drawing.FractalPainter
+import drawing.Painter
+import drawing.SelectionRect
+import drawing.convertation.Converter
 import drawing.convertation.Plane
-import math.Complex
+import math.fractals.Julia
 import kotlin.math.absoluteValue
 import kotlin.math.cos
+import kotlin.math.log2
 import kotlin.math.sin
 
 
 @Composable
 fun Julia(selectedPoint: Offset, fp: FractalPainter){
-    val plane = Plane(-2.0, 2.0, -2.0, 2.0, 0f, 0f)
-    val jpcomplex = math.fractals.Julia(Complex(selectedPoint.x.toDouble(), selectedPoint.y.toDouble()),  fp.fractal.function)
 
-    val color = remember { mutableStateOf("color1") }
-    val function = remember { mutableStateOf("Mandelbrot") }
-    val jp = remember { mutableStateOf( FractalPainter(jpcomplex){
+    val jp = remember { FractalPainter(Julia){
         if (it == 1f) Color.Black
         else {
             val r = sin(it*15f).absoluteValue
             val g = (sin(-8f*it)* cos(it*5f+12f)).absoluteValue
-            val b = sin(2f - cos(sin(18*-it))).absoluteValue
+            val b = log2(2f - cos(sin(18*-it)))
             Color(r, g, b)
         }
-    })
-    }
-    jp.value.plane = plane
+    }}
+    jp.plane = Plane(-2.0, 2.0, -2.0, 2.0, 0f, 0f)
     MaterialTheme {
-        DrawingPanel(jp, color, function){size ->
-            jp.value.width = size.width.toInt()
-            jp.value.height = size.height.toInt()
-            jp.value.refresh = true
+        DrawingPanel(jp){ size ->
+            jp.width = size.width.toInt()
+            jp.height = size.height.toInt()
+            jp.refresh = true
         }
+        SelectionPanel{
+            jp.plane?.let{ plane ->
+                val xMin = Converter.xScr2Crt(it.topLeft.x, plane)
+                val xMax = Converter.xScr2Crt(it.topLeft.x+it.size.width, plane)
+                val yMax = Converter.yScr2Crt(it.topLeft.y, plane)
+                val yMin = Converter.yScr2Crt(it.topLeft.y+it.size.height, plane)
+                plane.xMin = xMin
+                plane.xMax = xMax
+                plane.yMin = yMin
+                plane.yMax = yMax
+                jp.refresh = true
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SelectionPanel(
+    onSelected: (SelectionRect)->Unit
+) {
+    var rect by remember {mutableStateOf(SelectionRect(Offset.Zero))}
+    Canvas(Modifier.fillMaxSize().padding(8.dp).pointerInput(Unit){
+        detectDragGestures(
+            onDragStart = {
+                rect = SelectionRect(it)
+            },
+            onDrag = {
+                rect.addPoint(it)
+            },
+            onDragEnd = {
+                onSelected(rect)
+                rect = SelectionRect(Offset.Zero)
+            },
+            matcher = PointerMatcher.Primary)
+    }){
+        drawRect(Color(0f, 1f, 1f, 0.3f), rect.topLeft, rect.size)
+    }
+}
+
+@Composable
+fun DrawingPanel(
+    fp: Painter,
+    onResize: (Size)-> Unit = {},
+) {
+    Canvas(Modifier.fillMaxSize().padding(8.dp)) {
+        if(fp.width != size.width.toInt() || fp.height != size.height.toInt() )
+            onResize(size)
+
+        fp.paint(this)
     }
 }
