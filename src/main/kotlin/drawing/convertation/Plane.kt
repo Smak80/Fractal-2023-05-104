52b732d780138348d58e8ea27155ec05689c8208
Azal03 <Gajfutdinovaazalia@gmail.com>
2023-12-09 21:16:32 +0300

    В классе Plane добавлены 4 свойства.

--- src/main/kotlin/drawing/convertation/Plane.kt
+++ src/main/kotlin/drawing/convertation/Plane.kt
@@ -12,5 +12,13 @@ data class Plane(
         get() = width/(xMax-xMin)
     val yDen: Double
         get() = height/(yMax-yMin)
+    val xLen: Double
+        get() = xMax - xMin
+    val yLen: Double
+        get() = yMax - yMin
+    val dXY: Double
+        get() = xLen * 1.0 / yLen
+    val dWH: Double
+        get() = width * 1.0 / height
 
 }
