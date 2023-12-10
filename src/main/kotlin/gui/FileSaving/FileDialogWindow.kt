8c8dd7c34133cd23acff1f34388fdd65bee1c011
marat <marat-mmb@mail.ru>
2023-12-10 17:05:34 +0300

    Создано сохранение картинки. К Жюлиа пока не относится

--- src/main/kotlin/gui/FileSaving/FileDialogWindow.kt
+++ src/main/kotlin/gui/FileSaving/FileDialogWindow.kt
@@ -1,37 +1,52 @@
 package gui.FileSaving
 
-import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 import javax.swing.JFileChooser
 import javax.swing.JFrame
 import javax.swing.JOptionPane
+import javax.swing.WindowConstants.EXIT_ON_CLOSE
 import javax.swing.filechooser.FileNameExtensionFilter
 
 
-fun FileDialogWindow(Photo: BufferedImage) {
+fun FileDialogWindow(photo: BufferedImage) {
     val fileDialogFrame = JFrame()
+    fileDialogFrame.defaultCloseOperation = EXIT_ON_CLOSE
     val fileChooser = JFileChooser()
 
     val filter = FileNameExtensionFilter(
-        "Фотографии", "png", "jpg"
+        "Фотографии", "jpg"
     )
 
     fileChooser.fileFilter = filter
     fileChooser.dialogTitle = "Сохранение фрактала"
     fileChooser.fileSelectionMode = JFileChooser.FILES_ONLY;
-    val returnVAl = fileChooser.showSaveDialog(fileDialogFrame)
-    if (returnVAl == JFileChooser.APPROVE_OPTION ) {
+
+
+    var result = fileChooser.showSaveDialog(fileDialogFrame)
+
+
+    if (File("${fileChooser.selectedFile}.jpg").exists()) {
         JOptionPane.showMessageDialog(
             fileDialogFrame,
-            "Файл '" + fileChooser.getSelectedFile() +
-                    " ) сохранен"
-        );
-        ImageIO.write(Photo, "jpg", File("${fileChooser.getSelectedFile()}.jpg"))
+            "Файл '" + fileChooser.selectedFile +
+                    " существует"
+        )
+        fileChooser.showSaveDialog(fileDialogFrame)
     }
-
-
-
-
+    else{
+        try {
+            ImageIO.write(photo, "jpg", File("${fileChooser.selectedFile}.jpg"))
+            JOptionPane.showMessageDialog(
+                fileDialogFrame,
+                "Файл '" + fileChooser.selectedFile +
+                        " сохранен"
+            );
+        }
+        catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
 }
